5e19c6cd32b6971a3eaed539b8264b3b300f6c5e
gayeon <qkrrkdus1098@gmail.com>
2024-11-20 16:54:51 +0900

    first commit

---
+++ journal/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Image, StyleSheet, Text,Platform } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <Text>우왁 이거 진짜 오랜만이네예..</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
